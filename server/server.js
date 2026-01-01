f0710562f336b5d1cda6fe2568661a7cf9670b04
satyikpritam <satyik.pritam@joinsuperset.com>
2026-01-02 03:27:18 +0530

    Increased post size limit

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
